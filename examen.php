1f55db021d7dfe7d617ff1040aa698f6098f9078
ALvaro <you@example.com>
2021-10-19 14:30:21 +0200

    Este es el primer commit

---
+++ examen.php
@@ -0,0 +1,1 @@
+<?php echo '<p>ALvaro, 19/10/2021, Un repositorio de código es un lugar donde el código de una aplicación, de un programa cualquiera está almacenado y desde donde se puede distribuir.</p>'; ?>

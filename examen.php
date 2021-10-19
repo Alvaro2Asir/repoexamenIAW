e4af523256e282a4227fd9e2a0f0c253fece2094
ALvaro <you@example.com>
2021-10-19 14:35:09 +0200

    Este es el segundo commit

--- examen.php
+++ examen.php
@@ -1,1 +1,7 @@
-<?php echo '<p>ALvaro, 19/10/2021, Un repositorio de código es un lugar donde el código de una aplicación, de un programa cualquiera está almacenado y desde donde se puede distribuir.</p>'; ?>
+<body>
+	<head></head>
+	<body>
+		<?php echo '<p><h1>ALvaro</h1>, <h2>19/10/2021</h2>, <h3>Un repositorio de código es un lugar donde el código de una aplicación, de un programa cualquiera está almacenado y desde donde se puede distribuir</h3>.</p>'; ?>
+	</body>
+</body>
+

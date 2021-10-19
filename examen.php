806be6b17171f12ad733c0d1e82c2abcc117234b
ALvaro <you@example.com>
2021-10-19 14:39:36 +0200

    Este es el tercer commit

--- examen.php
+++ examen.php
@@ -1,7 +1,9 @@
 <body>
-	<head></head>
+	<head>
+		<link rel="stylesheet" href="examen.css">
+	</head>
 	<body>
-		<?php echo '<p><h1>ALvaro</h1>, <h2>19/10/2021</h2>, <h3>Un repositorio de código es un lugar donde el código de una aplicación, de un programa cualquiera está almacenado y desde donde se puede distribuir</h3>.</p>'; ?>
+		<?php echo '<p><h1 class="color">ALvaro</h1>, <h2>19/10/2021</h2>, <h3>Un repositorio de código es un lugar donde el código de una aplicación, de un programa cualquiera está almacenado y desde donde se puede distribuir</h3>.</p>'; ?>
 	</body>
 </body>
 

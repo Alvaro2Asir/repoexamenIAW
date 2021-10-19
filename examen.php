b806fde0945526969c1da50ef44ea78da0a73db2
ALvaro <you@example.com>
2021-10-19 14:43:57 +0200

    Este es el cuarto commit

--- examen.php
+++ examen.php
@@ -4,6 +4,7 @@
 	</head>
 	<body>
 		<?php echo '<p><h1 class="color">ALvaro</h1>, <h2>19/10/2021</h2>, <h3>Un repositorio de código es un lugar donde el código de una aplicación, de un programa cualquiera está almacenado y desde donde se puede distribuir</h3>.</p>'; ?>
+		<image src="hola.png">
 	</body>
 </body>
 
